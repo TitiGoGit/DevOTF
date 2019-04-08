bb6057e3ae7c1e7b3e13cb2722f88bf79e4cd604
TitiGit <quatrieme@msn.com>
2019-04-08 17:54:31 +0200

    dossier realisation ajouté, manque contact

---
+++ carte/anniversaire/js/anniversaire.js
@@ -0,0 +1,26 @@
+//&lt;![CDATA[
+
+jQuery(document).ready(function(){
+
+$('#dmcol').hide(0).delay(5000).show(500);
+});
+
+//]]&gt;
+
+jQuery(document).ready(function(){
+
+$('#ptmo').hide(0).delay(2000).show(500);
+});
+
+jQuery(document).ready(function(){
+
+$('#pmcol').hide(0).delay(12000).show(500);
+});
+
+/*jQuery(document).ready(function(){
+
+$('body').css(background-image: url(image/coeur.png))
+});*/
+
+
+
